371151b21233741db1914323428aeb3c58c0718b
maxwaslick <98866378+maxwaslick@users.noreply.github.com>
2022-04-12 13:49:36 -0400

    fix frontend a bit

--- backend/reviewsite/views.py
+++ backend/reviewsite/views.py
@@ -7,3 +7,15 @@ from .models import Rating
 class RatingView(viewsets.ModelViewSet):
   serializer_class = RatingSerializer
   queryset = Rating.objects.all()
+
+class ArtistView(viewsets.ModelViewSet):
+  serializer_class = ArtistSerializer
+  queryset = Artist.objects.all()
+
+class SongView(viewsets.ModelViewSet):
+  serializer_class = SongSerializer
+  queryset = Song.objects.all()
+
+class UserView(viewsets.ModelViewSet):
+  serializer_class = UserSerializer
+  queryset = User.objects.all()
